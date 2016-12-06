9b347e9344241ee6294098dbf5012ad3b8a6aa2f
vlad <kovtunovvladislav@gmail.com>
2016-12-06 10:23:46 +0200

    added some examples;

--- index.php
+++ index.php
@@ -1,181 +1,53 @@
 <?php
 echo "<h1>";
 
-// while
-/*$i = 1;
-while ($i <= 10)
-{
-    echo $i++ . '<br />';
-}
- */
-
-// do while
-/*$i = 0;
-do
-{
-    echo ++$i . '<br />';
-}
-while ($i < 5);
- */
-
-//sprintf
-/*$num = 5;
-$location = 'дереве';
-
-$format = '%d обезьян сидят на %s';
-echo sprintf($format, $num, $location);
- */
+//echo intval(42);                      // 42
+//echo intval(4.2);                     // 4
+//echo intval('42');                    // 42
+//echo intval('+42');                   // 42
+//echo intval('-42');                   // -42
 
 
-//var_exprot
-/*
-$a = array (1, 2, array ("a", "b", "c"));
-var_export($a);
- */
+//$var = '122.34343The';
+//$float_value_of_var = floatval($var);
+//echo $float_value_of_var; // 122.34343
 
 
-// Object
-/*class foo
-{
-    function do_foo()
-    {
-        echo "Doing foo.";
-    }
-}
+//$foo = "5bar"; // строка
+//$bar = true;   // булевое значение
+//settype($foo, "integer"); // $foo теперь 5   (целое)
+//settype($bar, "string");  // $bar теперь "1" (строка)
 
-$bar = new foo;
-$bar->do_foo();
- */
 
-// Resource
-//$handle = fopen('./index.php', 'r');
-//fclose($handle);
-//echo __FILE__;
+//$a    = 'car'; // $a - это строка
+//$a[0] = 'b';   // $a все еще строка
+//echo $a;       // bar
+
+
+//$foo = 10;   // $foo это целое число
+//$bar = (boolean) $foo;   // $bar это булев тип
+
+
+//function foo(&$bar)
+//{
+//    unset($bar);
+//    $bar = "blah";
+//}
+//$bar = 'something';
+//echo "$bar\n";
+//foo($bar);
+//echo "$bar\n";
+
+
+// Some information about git
+//https://git-scm.com/book/ru/v1/%D0%92%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%9E-%D0%BA%D0%BE%D0%BD%D1%82%D1%80%D0%BE%D0%BB%D0%B5-%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D0%B9
+//https://camo.githubusercontent.com/f2a574f39c54fec927c020221af0fced76f0e7db/68747470733a2f2f646f63732e676f6f676c652e636f6d2f64726177696e67732f642f317238334b3634544643744239386d424747463964324b5056656b556e6d6979396562665675676a726378302f7075623f773d36303626683d323930
+//git log --oneline --decorate --all --graph
 
 
 
-//abs
-//echo abs(-4.2) . '<br />';
-//echo abs(5) . '<br />';
-//echo abs(-5) . '<br />';
 
 
-//max — Возвращает наибольшее значение
-//echo max(1, 3, 5, 6, 7);
-//echo max(array(2, 4, 5));
-
-
-//min — Находит наименьшее значение
-//echo min(2, 3, 1, 6, 7);
-//echo "<br />" . min(array(2, 4, 5));
-
-
-//sqrt — Квадратный корень
-//echo sqrt(9);
-//echo sqrt(10);
-
-
-//pow — Возведение в степень
-//echo pow(2, 8); // int(256)
-
-//echo pow(49,1/2) . "<br />";
-
-//date
-//echo date('Y-m-d');
-//echo date("F j, Y, g:i a");                 // March 10, 2001, 5:16 pm
-//echo date("m.d.y");                         // 03.10.01
-//echo date("j, n, Y");                       // 10, 3, 2001
-//echo date("Ymd");                           // 20010310
-//echo date('h-i-s, j-m-y, it is w Day');     // 05-16-18, 10-03-01, 1631 1618 6 Satpm01
-//echo date('\i\t \i\s \t\h\e jS \d\a\y.');   // it is the 10th day.
-//echo date("D M j G:i:s T Y");               // Sat Mar 10 17:16:18 MST 2001
-//echo date('H:m:s \m \i\s\ \m\o\n\t\h');     // 17:03:18 m is month
-//echo date("H:i:s");                         // 17:16:18
-//echo date("Y-m-d H:i:s");                   // 2001-03-10 17:16:18 (формат MySQL DATETIME)
-
-
-//strtotime  Преобразует текстовое представление даты на английском языке в метку времени Unix
-//echo strtotime("now"), "\n";
-//echo strtotime("10 September 2000"), "\n";
-//echo strtotime("+1 day"), "\n";
-//echo strtotime("+1 week"), "\n";
-//echo strtotime("+1 week 2 days 4 hours 2 seconds"), "\n";
-//echo strtotime("next Thursday"), "\n";
-//echo strtotime("last Monday"), "\n";
-//echo date('Y-m-d : H-i-s', strtotime("+1 day"));
-
-//mktime   Возвращает метку времени Unix для заданной даты
-//echo date("M-d-Y", mktime(0, 0, 0, 12, 03, 1997));
-//echo date("M-d-Y", mktime(0, 0, 0, 01, 1, 1997));
-//echo date("M-d-Y", mktime(0, 0, 0, 1, 1, 1998));
-//echo date("M-d-Y", mktime(0, 0, 0, 1, 1, 98));
-
-
-//time — Возвращает текущую метку времени Unix
-//$nextWeek = time() + (7 * 24 * 60 * 60);
-//// 7 дней; 24 часа; 60 минут; 60 секунд
-//echo 'Сейчас:           '. date('Y-m-d') ."\n";
-//echo 'Следующая неделя: '. date('Y-m-d', $nextWeek) ."\n";
-
-
-// FUNCTION
-/**
- * Display firstName and secondName
- * @param string $name
- * @param string $secondName
- * @return string Information about full name for some person
- */
-function displayFullName($name, $secondName)
-{
-    $fullName = "My name is $name! <br />My secondname is $secondName!!<br /><br />";
-    return $fullName;
-}
-
-//echo displayFullName('Ivan', 'Ivanov');
-//echo displayFullName('Petr', 'Petrov');
-
-
-
-//func_num_args() — Возвращает количество аргументов, переданных функции
-function foo()
-{
-    $numargs = func_num_args();
-    echo "Количество аргументов: $numargs\n";
-}
-
-//foo(1, 2, 3);
-
-
-//func_get_arg() — Возвращает элемент из списка аргументов
-function foo1()
-{
-    $numargs = func_num_args();
-    echo "Количество аргументов: $numargs\n";
-    if ($numargs >= 2) {
-        echo "Второй аргумент: " . func_get_arg(1) . "\n";
-    }
-}
-
-//foo1(1, 21, 3);
-
-
-//func_get_args() — Возвращает массив, содержащий аргументы функции
-function foo2()
-{
-    $numargs = func_num_args();
-    echo "Количество аргументов: $numargs<br />";
-    if ($numargs >= 2) 
-    {
-        echo "Второй аргумент: " . func_get_arg(1) . "<br />";
-    }
-    $arg_list = func_get_args();
-    for ($i = 0; $i < $numargs; $i++) 
-    {
-        echo "Аргумент №$i: " . $arg_list[$i] . "<br />";
-    }
-}
-
-foo2(11, 21, 31);
 
 
 echo "</h1>";
