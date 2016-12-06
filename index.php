9d85c7fed4111538c359395208e984ad45f74138
vlad <kovtunovvladislav@gmail.com>
2016-12-06 10:38:20 +0200

    added form and string function;

--- index.php
+++ index.php
@@ -43,12 +43,33 @@ echo "<h1>";
 //https://git-scm.com/book/ru/v1/%D0%92%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%9E-%D0%BA%D0%BE%D0%BD%D1%82%D1%80%D0%BE%D0%BB%D0%B5-%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D0%B9
 //https://camo.githubusercontent.com/f2a574f39c54fec927c020221af0fced76f0e7db/68747470733a2f2f646f63732e676f6f676c652e636f6d2f64726177696e67732f642f317238334b3634544643744239386d424747463964324b5056656b556e6d6979396562665675676a726378302f7075623f773d36303626683d323930
 //git log --oneline --decorate --all --graph
+//git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
+
+
+//$new = htmlspecialchars("<a href='test'>Test</a>", ENT_QUOTES);
+//echo $new; // &lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;
+
+
+//$str = "<p>this -&gt; &quot;</p>\n";
+//echo htmlspecialchars_decode($str);
+
+
+//$form = <<<FORM
+//<form action="action.php" method="post">
+// <p>Ваше имя: <input type="text" name="name" /></p>
+// <p>Ваш возраст: <input type="text" name="age" /></p>
+// <p><input type="submit" /></p>
+//</form>
+//FORM;
+//
+//echo $form;
 
 
 
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!! String function !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+//echo nl2br("foo - это вам не\n bar");
 
 
 
 echo "</h1>";
-
