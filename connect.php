439ac6264bfa123a2f7a0d555f1ad1d20e70a022
vlad <chimgan@list.ru>
2017-01-06 00:17:57 +0200

    add encoding line;

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$link = mysqli_connect('localhost:3306', 'root', 'password', 'test');
-if (!$link) 
-{
-    die('Ошибка соединения: ' . mysqli_error());
-}
-echo 'Успешно соединились';
-mysqli_close($link);
--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
+echo "<meta charset=\"UTF-8\">";
 echo "<h1>";
 
 $link = mysqli_connect('localhost', 'root', 'password', 'test');
 if (!$link)
 {
-    die('Ошибка соединения: ' . mysqli_error());
+    die('Ошибка соединения: ' . mysqli_error($link));
 }
 echo 'Успешно соединились';
 mysqli_close($link);
