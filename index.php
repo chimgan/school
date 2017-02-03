00bbeaf44545786c8bc1c8ebd342cf4e5a3acea3
vkovtunov <vkovtunov@acceptic.com>
2017-02-03 10:47:41 +0200

    lesson 15 bash;

--- index.php
+++ index.php
@@ -262,6 +262,9 @@
 //sudo mv composer.phar /usr/local/bin/composer
 //composer global require "laravel/installer"
 //composer create-project --prefer-dist laravel/laravel myTestProject
+#.bashrc
+//export PATH="$HOME/.config/composer/vendor/bin:$PATH"
+//source .bashrc
 
 echo "<h1>";
 
